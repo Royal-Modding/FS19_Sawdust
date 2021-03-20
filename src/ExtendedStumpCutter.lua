acb6298a9ad4117af4d4a02b94603e04d8b15dc0
fcelsa <fabio.celsa@gmail.com>
2021-03-20 23:09:16 +0100

    Extended spec ok

--- src/ExtendedStumpCutter.lua
+++ src/ExtendedStumpCutter.lua
@@ -1,3 +1,45 @@
+---${title}
 
+---@author ${author}
+---@version r_version_r
+---@date 05/11/2020
 
+---@class ExtendedStumpCutter
+---@field spec_stumpCutter any
+---@field isServer any
+---@field getIsTurnedOn function
+ExtendedStumpCutter = {}
+ExtendedStumpCutter.MOD_NAME = g_currentModName
+ExtendedStumpCutter.SPEC_TABLE_NAME = string.format("spec_%s.extendedStumpCutter", ExtendedStumpCutter.MOD_NAME)
 
+function ExtendedStumpCutter.prerequisitesPresent(specializations)
+    return true
+end
+
+function ExtendedStumpCutter.registerEventListeners(vehicleType)
+    SpecializationUtil.registerEventListener(vehicleType, "onUpdateTick", ExtendedStumpCutter)
+    SpecializationUtil.registerEventListener(vehicleType, "onLoad", ExtendedStumpCutter)
+end
+
+function ExtendedStumpCutter:onLoad()
+    local spec = self[ExtendedStumpCutter.SPEC_TABLE_NAME]
+    spec.sawdustBuffer = 0
+    spec.sawdustBufferMax = g_densityMapHeightManager:getMinValidLiterValue(FillType.WOODCHIPS)
+end
+
+function ExtendedStumpCutter:onUpdateTick(dt, isActiveForInput, isActiveForInputIgnoreSelection, isSelected)
+    ---@type any
+    local spec = self[ExtendedStumpCutter.SPEC_TABLE_NAME]
+    if self.isServer then
+        if self:getIsTurnedOn() and self.spec_stumpCutter.curSplitShape then
+            local delta = (10 / 1000) * dt
+            delta = delta * g_sawdust.sawdustScale
+            spec.sawdustBuffer = spec.sawdustBuffer + delta
+        end
+        if spec.sawdustBuffer >= spec.sawdustBufferMax then
+            local x, y, z = getWorldTranslation(self.spec_stumpCutter.cutNode)
+            g_sawdust:addChipToGround(x, y, z, spec.sawdustBuffer)
+            spec.sawdustBuffer = 0
+        end
+    end
+end
--- src/ExtendedTreeSaw.lua
+++ src/ExtendedTreeSaw.lua
@@ -1,2 +1,44 @@
+---${title}
 
+---@author ${author}
+---@version r_version_r
+---@date 05/11/2020
 
+---@class ExtendedTreeSaw
+---@field spec_treeSaw any
+---@field isServer any
+ExtendedTreeSaw = {}
+ExtendedTreeSaw.MOD_NAME = g_currentModName
+ExtendedTreeSaw.SPEC_TABLE_NAME = string.format("spec_%s.extendedTreeSaw", ExtendedTreeSaw.MOD_NAME)
+
+function ExtendedTreeSaw.prerequisitesPresent(specializations)
+    return true
+end
+
+function ExtendedTreeSaw.registerEventListeners(vehicleType)
+    SpecializationUtil.registerEventListener(vehicleType, "onUpdateTick", ExtendedTreeSaw)
+    SpecializationUtil.registerEventListener(vehicleType, "onLoad", ExtendedTreeSaw)
+end
+
+function ExtendedTreeSaw:onLoad()
+    local spec = self[ExtendedTreeSaw.SPEC_TABLE_NAME]
+    spec.sawdustBuffer = 0
+    spec.sawdustBufferMax = g_densityMapHeightManager:getMinValidLiterValue(FillType.WOODCHIPS)
+end
+
+function ExtendedTreeSaw:onUpdateTick(dt, isActiveForInput, isActiveForInputIgnoreSelection, isSelected)
+    ---@type any
+    local spec = self[ExtendedTreeSaw.SPEC_TABLE_NAME]
+    if self.isServer then
+        if self.spec_treeSaw.isCutting then
+            local delta = (50 / 1000) * dt
+            delta = delta * g_sawdust.sawdustScale
+            spec.sawdustBuffer = spec.sawdustBuffer + delta
+        end
+        if spec.sawdustBuffer >= spec.sawdustBufferMax then
+            local x, y, z = getWorldTranslation(self.spec_treeSaw.cutNode)
+            g_sawdust:addChipToGround(x, y, z, spec.sawdustBuffer)
+            spec.sawdustBuffer = 0
+        end
+    end
+end
